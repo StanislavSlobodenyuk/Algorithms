cee26db555c871c56bb08a4eb2a949ea320ff9f6
StanislavSlobodenyuk <116057509+StanislavSlobodenyuk@users.noreply.github.com>
2023-07-04 13:07:00 +0300

    Добавьте файлы проекта.

---
+++ BFS_algorithm/Graph.cs
@@ -0,0 +1,51 @@
+﻿
+namespace algorithm_with_graphs
+{
+    internal class Graph
+    {
+        Dictionary<int, Dictionary<int, int>> adjacencyList;
+
+        public Graph()
+        {
+            adjacencyList = new Dictionary<int, Dictionary<int, int>>() { };
+        }
+
+        public void  AddVertex(int vertex)
+        {
+            if (!adjacencyList.ContainsKey(vertex))
+            {
+                adjacencyList[vertex] = new Dictionary<int, int>();
+            }
+        }
+
+        public void AddEdge(int sourse, int destination, int weight)
+        {
+            if (!adjacencyList.ContainsKey(sourse))
+            {
+                adjacencyList[sourse] = new Dictionary<int, int>();
+            }
+
+            if (!adjacencyList.ContainsKey(destination))
+            {
+                adjacencyList[destination] = new Dictionary<int, int>();
+            }
+
+            adjacencyList[sourse][destination] = weight;
+            /* adjacencyList[destination][sourse] = weight;*/ // якщо граф НЕ направлений, цей рядок повинен бути закоментований або видалений
+        }
+
+        public void PrintGraph()
+        {
+            foreach (var vertex in adjacencyList.Keys)
+            {
+                Console.Write(vertex + ": ");
+
+                foreach (var neighbor in adjacencyList[vertex])
+                {
+                    Console.Write(neighbor.Key + "(" + neighbor.Value + ") ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ BFS_algorithm/Program.cs
@@ -0,0 +1,17 @@
+﻿using algorithm_with_graphs;
+
+Graph graph = new Graph();
+
+graph.AddVertex(1);
+graph.AddVertex(2);
+graph.AddVertex(3);
+graph.AddVertex(4);
+graph.AddVertex(5);
+
+graph.AddEdge(1, 2, 10);
+graph.AddEdge(1, 3, 5);
+graph.AddEdge(2, 4, 8);
+graph.AddEdge(3, 4, 7);
+graph.AddEdge(4, 5, 3);
+
+graph.PrintGraph();
